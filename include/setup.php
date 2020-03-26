4ad4568b6893415cebf38d6c7213333757444ed4
Rodrigo Viturino <Rodrigo@upsites.digital>
2020-03-26 16:04:13 -0300

    Header - Footer : Ajustes geral

--- include/setup.php
+++ include/setup.php
@@ -9,7 +9,7 @@ function up_theme_styles(){
 
     // JAVASCRIPT
         wp_enqueue_script('jquery_js', get_template_directory_uri() . '/src/js/libs/jquery-3.4.1.min.js', [], true);
-        wp_enqueue_script('theme_js', get_template_directory_uri() . '/public/theme.min.js', ['jquery'], $version, true);
+        wp_enqueue_script('theme_js', get_template_directory_uri() . '/public/theme.min.js', ['jquery'], true);
 }
 
 
